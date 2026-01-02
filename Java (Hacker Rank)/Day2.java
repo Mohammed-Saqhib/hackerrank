f123a720b4900c15ba707784ed22ff904f4acc53
Mohammed Saqhib <125188740+Mohammed-Saqhib@users.noreply.github.com>
2026-01-02 18:27:35 +0000

    Add problem description comment to Day2.java

---
+++ Java (Hacker Rank)/Day2.java
@@ -0,0 +1,43 @@
+ /*
+     * PROBLEM: Weird or Not Weird
+     * Determine whether a given integer N is "Weird" or "Not Weird" based on:
+     * - If N is odd: print "Weird"
+     * - If N is even and 2 <= N <= 5: print "Not Weird"
+     * - If N is even and 6 <= N <= 20: print "Weird"
+     * - If N is even and N > 20: print "Not Weird"
+     */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+            if (N%2!=0){
+                System.out.println("Weird");
+            }
+            else if (N>=2 && N<=5){
+                System.err.println("Not wierd");
+            }
+            else if (N>=6 && N<=20){
+                System.err.println("Weird");
+            }
+            else {
+                System.out.println("Not Weird");
+            }
+            
+        
+        scanner.close();
+    }
+}
