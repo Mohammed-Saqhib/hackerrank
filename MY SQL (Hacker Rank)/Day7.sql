887213b81246964bd7ddd52810ac840822678d35
Mohammed Saqhib <125188740+Mohammed-Saqhib@users.noreply.github.com>
2026-01-07 15:32:21 +0000

    query 2

--- MY SQL (Hacker Rank)/Day7.sql
+++ MY SQL (Hacker Rank)/Day7.sql
@@ -1,3 +1,4 @@
+-- Problem: Weather Observation Station 4 - Find the difference between the total number of city entries and the number of distinct city entries in the station table (i.e., the number of duplicate cities).
 -- This query calculates the number of duplicate city entries in the station table by subtracting the count of distinct cities from the total count of cities.
 select count(city) - count(distinct city) from  station;
 
@@ -5,3 +6,13 @@ select count(city) - count(distinct city) from  station;
 
 
 ----query 2
+-- Problem: Weather Observation Station 5 - Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths.
+-- Shortest city name:
+SELECT city , length(city) from station
+order by length(city)asc,city ASC
+LIMIT 1;
+
+-- Longest city name:
+SELECT city , length(city) from station
+order by length(city)DESC,city DESC
+LIMIT 1;
