d5942f0d7e0c9568a7db3ec953c0a12680597449
Mohammed Saqhib <125188740+Mohammed-Saqhib@users.noreply.github.com>
2026-01-07 15:17:09 +0000

    adding comment's

--- MY SQL (Hacker Rank)/Day7.sql
+++ MY SQL (Hacker Rank)/Day7.sql
@@ -1,1 +1,2 @@
+-- This query calculates the number of duplicate city entries in the station table by subtracting the count of distinct cities from the total count of cities.
 select count(city) - count(distinct city) from  station;
